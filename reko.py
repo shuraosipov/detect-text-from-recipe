6c09eb3a5e74f9a50fc7a4b3b46f6779045061bd
Alexander Osipov <shuraosipov@gmail.com>
2019-12-29 16:53:42 -0500

    added JupyterLab notebook to this project

--- reko.py
+++ reko.py
@@ -1,32 +1,31 @@
-# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-developer-guide/blob/master/LICENSE-SAMPLECODE.)
+#Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-developer-guide/blob/master/LICENSE-SAMPLECODE.)
 
 import boto3
-import json
-
 
 def detect_text(photo, bucket):
-    client = boto3.client('rekognition')
 
-    response = client.detect_text(Image={'S3Object': {'Bucket': bucket, 'Name': photo}})
+    client=boto3.client('rekognition')
 
-    textDetections = response['TextDetections']
-    print('Detected text\n----------')
+    response=client.detect_text(Image={'S3Object':{'Bucket':bucket,'Name':photo}})
+                        
+    textDetections=response['TextDetections']
+    print ('Detected text\n----------')
     for text in textDetections:
-        print('Detected text:' + text['DetectedText'])
-        print('Confidence: ' + "{:.2f}".format(text['Confidence']) + "%")
-        print('Id: {}'.format(text['Id']))
-        if 'ParentId' in text:
-            print('Parent Id: {}'.format(text['ParentId']))
-        print('Type:' + text['Type'])
-        print()
+            print ('Detected text:' + text['DetectedText'])
+            print ('Confidence: ' + "{:.2f}".format(text['Confidence']) + "%")
+            print ('Id: {}'.format(text['Id']))
+            if 'ParentId' in text:
+                print ('Parent Id: {}'.format(text['ParentId']))
+            print ('Type:' + text['Type'])
+            print()
     return len(textDetections)
 
-
 def main():
-    bucket = 'shuraosipov-recognition'
-    photo = 'photo_2019-12-15_13-59-26.jpg'
-    text_count = detect_text(photo, bucket)
+
+    bucket='bucket'
+    photo='photo'
+    text_count=detect_text(photo,bucket)
     print("Text detected: " + str(text_count))
 
 
